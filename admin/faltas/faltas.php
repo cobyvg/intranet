4d124c96d5e09ba9502778b3ab948c85df180b97
mgarcia39 <mgarcia39@gmail.com>
2013-11-26 20:04:37 +0100

    Error tonto

--- admin/faltas/faltas.php
+++ admin/faltas/faltas.php
@@ -69,7 +69,7 @@ $result = mysql_query($SQL);
         {
 			echo '<br /><div align="center"><div class="alert alert-warning alert-block fade in" style="max-width:500px;text-align:left">
             <button type="button" class="close" data-dismiss="alert">&times;</button>
-			<h5>ATENCIÓN:</h5>
+			<h5>ATENCIï¿½N:</h5>
 No hay registros coincidentes, bien porque te has equivocado
         al introducir los datos, bien porque ningun dato se ajusta a tus criterios.
 		</div></div><br />';
@@ -83,53 +83,3 @@ No hay registros coincidentes, bien porque te has equivocado
 </div>
 </body>
 </html>
-t' name='f1' class="form-inline">
-<div class="row-fluid">
-  <div class="span4">
-  <div class="well well-large pull-right"  style="width:340px;">
-  <legend>Resumen de faltas de un Grupo.</legend>
-<br />
-  <h6>Selecciona Nivel y Grupo</h6>
-    <label> Nivel:
-    <SELECT name="nivel" onChange="submit()" class="input-mini"  style="display:inline">
-      <OPTION><? echo $nivel;?></OPTION>
-      <? nivel();?>
-    </SELECT>
-  </label>
-  &nbsp;&nbsp;&nbsp;&nbsp;
-  <label style="display:inline">
-  Grupo:
-  <SELECT name="grupo" onChange="submit()" class="input-mini"  style="display:inline">
-    <OPTION><? echo $grupo;?></OPTION>
-    <? grupo($nivel);?>
-  </SELECT>
-  </label>
-  <hr>
-   <label>
-  Mes:&nbsp;
-  <SELECT name='mes' class="input-mini">
-    <OPTION></OPTION>
-    <?
-	for($i=1;$i<13;$i++){
-	echo "<OPTION>$i</OPTION>";	
-	}
-	?>    
-  </SELECT>
-  <!-- <INPUT name="mes" type="text" value="<? echo date(m); ?>" class="input-mini" maxlength="2" >-->
-  </label>
-  <br />
-  <label>
-  Falta:
-  <SELECT name='FALTA' class="input-mini">
-    <OPTION>F</OPTION>
-    <OPTION>J</OPTION>
-  </SELECT>
-  </label>
-  <br />
-  <label>
-  N&uacute;mero m&iacute;nimo de
-  Faltas
-  <INPUT name="numero2" type="text" class="input-mini" maxlength="3" alt="Mes" value="1">
-  </label>
-  <hr />
-  <input name="submit1
