356d56b688c69bc7b1ae6824d491fa67945cb037
Rubén López Herrera <rubenlh91@gmail.com>
2014-08-22 20:30:40 +0200

    Búsquedas en la intranet

--- buscar.php
+++ buscar.php
@@ -1,7 +1,7 @@
-<legend><span class="fa fa-search fa-fw"></span> Buscar alumno</legend>
+<legend><span class="fa fa-search fa-fw"></span> Buscar...</legend>
 
 <form method="get" action="admin/noticias/buscar.php">
-	<input type="text" class="form-control" name="q" id="buscarAlumnos" onkeyup="javascript:buscar('list_alumnos',this.value);" placeholder="Buscar...">
+	<input type="text" class="form-control" name="q" id="buscarAlumnos" onkeyup="javascript:buscar('list_alumnos',this.value);" placeholder="Buscar alumnos, noticias...">
 </form>
 
 <div id="list_alumnos"></div>
