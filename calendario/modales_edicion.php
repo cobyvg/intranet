a924c53e978aed671fb5e8a3132cf040784e4904
mgarcia39 <mgarcia39@gmail.com>
2015-03-08 18:21:18 +0100

    Solución de errores

--- calendario/modales_edicion.php
+++ calendario/modales_edicion.php
@@ -165,17 +165,17 @@ while ($calendario1 = mysqli_fetch_assoc($result_calendarios1)) {
 		        			
 		        			$result_cuaderno = mysqli_query($db_con, "SELECT id FROM notas_cuaderno WHERE nombre='".$eventos1['nombre']."' AND fecha='".substr($eventos1['fechareg'], 0, 10)."' AND curso='".$eventos1['unidades']."'");
 		        			
-		        			echo '<div class="form-group">
+		        			/*echo '<div class="form-group">
 		        				<div class="checkbox">
 		        					<label for="cmp_cuaderno">
-		        						<input type="checkbox" id="cmp_cuaderno" name="cmp_cuaderno" value="1"';
+		        						<input type="checkbox" id="cmp_cuaderno" name="cmp_cuaderno" value="1"';*/
 		        			if (mysqli_num_rows($result_cuaderno)) echo ' checked disabled'; 
-		        			echo '> Crear columna en mi cuaderno de notas<br>
+		        			/*echo '> Crear columna en mi cuaderno de notas<br>
 		        						<small class="text-muted">Se creará una columna de tipo numérico y no visible para las familias. Puede modificar estos valores en el cuaderno de notas.</small><br>
 		        						<small class="text-danger">Importante: Si la asignatura tiene desdoble de unidades, debe marcar todas las unidades afectadas. Deben ser del mismo curso y tener el mismo nombre de asignatura.</small>
 		        					</label>
 		        				</div>
-		        			</div>';
+		        			</div>';*/
 		        		endif;
 		        		echo '</div>
 		        						        				        		
