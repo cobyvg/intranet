85ca440d57ed8125928bc7127f3b18e2f15c803f
Rubén López Herrera <rubenlh91@gmail.com>
2015-03-18 10:57:33 +0100

    Documentación obsoleta

--- TIC/menu.php
+++ TIC/menu.php
@@ -5,7 +5,6 @@
 			<li<?php echo (strstr($_SERVER['REQUEST_URI'],'incidencias.php')==TRUE) ? ' class="active"' : ''; ?>><a href="incidencias.php">Listado de incidencias</a></li>
 			<li<?php echo (strstr($_SERVER['REQUEST_URI'],'perfiles_alumnos.php')==TRUE) ? ' class="active"' : ''; ?>><a href="perfiles_alumnos.php">Perfiles alumnos</a></li>
 			<li<?php echo (strstr($_SERVER['REQUEST_URI'],'perfiles_profesores.php')==TRUE) ? ' class="active"' : ''; ?>><a href="perfiles_profesores.php">Perfiles profesores</a></li>
-			<li<?php echo (strstr($_SERVER['REQUEST_URI'],'documentos.php')==TRUE) ? ' class="active"' : ''; ?>><a href="documentos.php">Documentos y manuales</a></li>
 			<li<?php echo (strstr($_SERVER['REQUEST_URI'],'protocolo.php')==TRUE) ? ' class="active"' : ''; ?>><a href="protocolo.php">Protocolo de uso</a></li>
 			<li<?php echo (strstr($_SERVER['REQUEST_URI'],'estadisticas.php')==TRUE) ? ' class="active"' : ''; ?>><a href="estadisticas.php">Estadísticas TIC</a></li>
 		</ul>
