836e06f3fec5d8fc1714700c4c3321482a3187ba
mgarcia39 <mgarcia39@gmail.com>
2015-04-29 13:45:11 +0200

    Solución de errores

--- admin/biblioteca/morosos.php
+++ admin/biblioteca/morosos.php
@@ -35,7 +35,7 @@ include("menu.php");
 <div class="container">
 <div class="row">
 <div class="page-header">
-<h2>Biblioteca <small> Edición de morosos</small></h2>
+<h2>Biblioteca <small> EdiciÃ³n de morosos</small></h2>
 </div>
 <br>
 
@@ -44,17 +44,17 @@ if(isset($_FILES['archivo'])){
 	mysqli_query($db_con,"create table morosos_tmp select * from morosos");
 	mysqli_query($db_con,"truncate table morosos_tmp");
 	$archivo = $_FILES['archivo'];
-	$db_con = mysqli_connect($db_host, $db_user, $db_pass) or die("Error de conexión");
+	$db_con = mysqli_connect($db_host, $db_user, $db_pass) or die("Error de conexiÃ³n");
 	mysqli_select_db($db_con, $db);
 	ini_set('auto_detect_line_endings',TRUE);
 	$handle = fopen ($_FILES['archivo']['tmp_name'] , 'r' ) or die
 	('<div align="center"><div class="alert alert-danger alert-block fade in">
             <button type="button" class="close" data-dismiss="alert">&times;</button>
-			<legend>ATENCIÓN:</legend>
-No se ha podido abrir el archivo exportado. O bien te has olvidado de enviarlo o el archivo está corrompido.
+			<legend>ATENCIÃ“N:</legend>
+No se ha podido abrir el archivo exportado. O bien te has olvidado de enviarlo o el archivo estÃ¡ corrompido.
 </div></div><br />
 <div align="center">
-  <input type="button" value="Volver atrás" name="boton" onClick="history.back(2)" class="btn btn-inverse" />
+  <input type="button" value="Volver atrÃ¡s" name="boton" onClick="history.back(2)" class="btn btn-inverse" />
 </div>'); 
 	while (($data1 = fgetcsv($handle, 1000, ";")) !== FALSE)
 	{
@@ -67,32 +67,32 @@ No se ha podido abrir el archivo exportado. O bien te has olvidado de enviarlo o
 
 		$dup = mysqli_query($db_con, "select * from morosos where curso = '$data1[0]' and apellidos = '$data1[1]' and nombre = '$data1[2]' and ejemplar = '$data1[3]' and devolucion = '$fecha_ed'");
 
-		if (mysqli_num_rows($dup)==0) {
-			$datos1 = mysqli_query($db_con, "INSERT INTO morosos (curso, apellidos, nombre, ejemplar, devolucion, hoy) VALUES ('". $data1[0]. "','". $data1[1]. "','". $data1[2] . "','". $data1[3] ."','". $fecha_ed ."', '".$hoy."')");
-		}
-		mysqli_query($db_con, "delete from morosos where apellidos = '' and nombre = '' and ejemplar = ''");
-	}
-
-	fclose($handle);
-	$borrar0 = mysqli_query($db_con, "delete from morosos where curso = 'ANT' or curso='' or curso='$nombre_del_centro' or curso like 'Abies%'");
-	
-	$del = mysqli_query($db_con, "select curso, apellidos, nombre, ejemplar, devolución from morosos_tmp");
-	while ($delete = mysqli_fetch_array($del)) {
-		$dup = mysqli_query($db_con, "select * from morosos where curso = '$delete[0]' and apellidos = '$delete[1]' and nombre = '$delete[2]' and ejemplar = '$delete[3]' and devolucion = '$delete[4]'");
 		if (mysqli_num_rows($dup)>0) {}
 		else{
-			mysqli_query($db_con, "delete from morosos where curso = '$delete[0]' and apellidos = '$delete[1]' and nombre = '$delete[2]' and ejemplar = '$delete[3]' and devolucion = '$delete[4]'");	
+			$datos1 = mysqli_query($db_con, "INSERT INTO morosos (curso, apellidos, nombre, ejemplar, devolucion, hoy) VALUES ('". $data1[0]. "','". $data1[1]. "','". $data1[2] . "','". $data1[3] ."','". $fecha_ed ."', '".$hoy."')");
 		}
 	}
 
-		if (mysqli_num_rows($dup)==0) {
-			$datos1 = mysqli_query($db_con, "INSERT INTO morosos (curso, apellidos, nombre, ejemplar, devolucion, hoy) VALUES ('". $data1[0]. "','". $data1[1]. "','". $data1[2] . "','". $data1[3] ."','". $fecha_ed ."', '".$hoy."')");
+	mysqli_query($db_con, "delete from morosos where apellidos = '' and nombre = '' and ejemplar = ''");
+	mysqli_query($db_con, "delete from morosos_tmp where apellidos = '' and nombre = '' and ejemplar = ''");
+
+	fclose($handle);
+	
+	$del = mysqli_query($db_con, "select curso, apellidos, nombre, ejemplar, devolucion from morosos");
+	while ($delete = mysqli_fetch_array($del)) {
+		$dup = mysqli_query($db_con, "select * from morosos_tmp where curso = '$delete[0]' and apellidos = '$delete[1]' and nombre = '$delete[2]' and ejemplar = '$delete[3]' and devolucion = '$delete[4]'");
+		if (mysqli_num_rows($dup)>0) {}
+		else{
+                    mysqli_query($db_con, "delete from morosos where curso = '$delete[0]' and apellidos = '$delete[1]' and nombre = '$delete[2]' and ejemplar = '$delete[3]' and devolucion = '$delete[4]'");
 		}
-	$borrar1 = mysqli_query($db_con, "delete from morosos where curso = 'ANT' or curso='' or curso='$nombre_del_centro' or curso like 'Abies%'");
+	}
+
+		
+	$borrar1 = mysqli_query($db_con, "delete from morosos where curso='Informe' or curso like 'Abies%' or apellidos like 'DepÃ³sito'");
 	
 	?>
 <div align="center">
-<div class="alert alert-success alert-block fade in"><legend>ATENCI&Oacute;N:</legendh5>
+<div class="alert alert-success alert-block fade in"><legend>ATENCI&Oacute;N:</legend>
 La actualizaci&oacute;n se ha realizado con &eacute;xito. Vuelve
 atr&aacute;s y compru&eacute;balo. </div>
 </div>
