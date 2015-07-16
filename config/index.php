8170aab65b5b9bfb4f5028936fa9c69e2310da66
Rubén López Herrera <rubenlh91@gmail.com>
2015-07-16 22:37:35 +0200

    Solución de errores

--- config/index.php
+++ config/index.php
@@ -160,7 +160,7 @@ include("tabla.php");
     </footer>
 	
 	
-  <script src="../js/jquery-1.11.2.min.js"></script>  
+  <script src="../js/jquery-1.11.3.min.js"></script>  
   <script src="../js/bootstrap.min.js"></script>
 
 	<script language="javascript">
