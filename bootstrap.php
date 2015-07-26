65924547e497feb8c803a1bc7e8a9b45a4e62869
Rubén López Herrera <rubenlh91@gmail.com>
2015-07-26 19:37:58 +0200

    Bootstrap

--- bootstrap.php
+++ bootstrap.php
@@ -60,4 +60,44 @@ registraPagina($_SERVER['REQUEST_URI'],$db_host,$db_user,$db_pass,$db);
 
 // Ver como usuario
 if($_SESSION['profi'] == 'admin') $_SESSION['user_admin'] = 1;
-if(isset($_SESSION['user_admin']) && isset($_POST['view_as_user'])) $_SESSION['profi'] = $_POST['view_as_user'];
+if(isset($_SESSION['user_admin']) && isset($_POST['view_as_user'])) {
+	$_SESSION['profi'] = $_POST['view_as_user'];
+	$profe = $_SESSION['profi'];
+	
+	// Variables de sesión del cargo del Profesor
+	$cargo0 = mysqli_query($db_con, "select cargo, departamento, idea from departamentos where nombre = '$profe'" );
+	$cargo1 = mysqli_fetch_array ( $cargo0 );
+	$_SESSION ['cargo'] = $cargo1 [0];
+	$carg = $_SESSION ['cargo'];
+	$_SESSION ['dpt'] = $cargo1 [1];
+	if (isset($_POST['idea'])) {}
+	else{
+	$_SESSION ['ide'] = $cargo1 [2];
+	}
+		
+	// Si es tutor
+	if (stristr ( $_SESSION ['cargo'], '2' ) == TRUE) {
+		$result = mysqli_query($db_con, "select distinct unidad from FTUTORES where tutor = '$profe'" );
+		$row = mysqli_fetch_array ( $result );
+		$_SESSION ['mod_tutoria']['tutor'] = $profe;
+		$_SESSION ['mod_tutoria']['unidad'] = $row [0];
+	}
+
+	// Si tiene Horario
+	$cur0 = mysqli_query($db_con, "SELECT distinct prof FROM horw where prof = '$profe'" );
+	$cur1 = mysqli_num_rows ( $cur0 );
+	$_SESSION ['n_cursos'] = $cur1;
+	
+	// Si tiene tema personalizado
+	$res = mysqli_query($db_con, "select distinct tema, fondo from temas where idea = '".$_SESSION ['ide']."'" );
+	if (mysqli_num_rows($res)>0) {
+		$ro = mysqli_fetch_array ( $res );
+		$_SESSION ['tema'] = $ro[0];
+		$_SESSION ['fondo'] = $ro[1];
+	}
+	else{
+		$_SESSION ['tema']="bootstrap.min.css";
+		$_SESSION ['fondo'] = "navbar-default";
+	}
+	
+}
