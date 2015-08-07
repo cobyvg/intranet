39509c97266542f84392b2d593b50377ae67b213
Rubén López Herrera <rubenlh91@gmail.com>
2015-08-07 16:49:32 +0200

    Más datos del Administrador

--- config/index.php
+++ config/index.php
@@ -229,6 +229,7 @@ if (isset($_POST['instalar']))
 				mysqli_query($db_con, "UPDATE c_profes SET pass='$pass_sha1', PROFESOR='Administrador', dni='$pass_admin' WHERE idea='admin' LIMIT 1");
 				mysqli_query($db_con, "UPDATE departamentos SET NOMBRE='Administrador', DNI='$pass_admin', DEPARTAMENTO='Admin', CARGO='1' WHERE idea='admin' LIMIT 1");
 				mysqli_query($db_con, "UPDATE calendario_categorias SET nombre='Administrador', fecha='".date('Y-m-d')."' WHERE nombre='admin' AND profesor='admin' LIMIT 1");
+				mysqli_query($db_con, "UPDATE noticias SET contact='Administrador' WHERE contact='admin'");
 				mysqli_query($db_con, "UPDATE mens_texto SET origen='Administrador' WHERE origen='admin'");
 				mysqli_query($db_con, "UPDATE mens_profes SET profesor='Administrador' WHERE profesor='admin'");
 				mysqli_query($db_con, "UPDATE reg_intranet SET profesor='Administrador' WHERE profesor='admin'");
