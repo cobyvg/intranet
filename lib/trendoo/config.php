2760ce28cb4b24c949f12fae49b0ff1f58aec2f5
Rubén López Herrera <rubenlh91@gmail.com>
2015-08-12 16:40:51 +0200

    Config Trendoo

--- lib/trendoo/config.php
+++ lib/trendoo/config.php
@@ -1,7 +1,7 @@
 <?php
 define('TRENDOO_HOSTNAME','api.trendoo.net');
-define('TRENDOO_USERNAME','instituto@iesmonterroso.org');
-define('TRENDOO_PASSWORD','PTczg5Y9');
+define('TRENDOO_USERNAME',$config['mod_sms_user']);
+define('TRENDOO_PASSWORD',$config['mod_sms_pass']);
 define('TRENDOO_DEFAULT_PORT',80);
 define('TRENDOO_PROXY','');
 define('TRENDOO_PROXY_PORT',8080);
