1e0911628c2d57ae1929d0f48110489acb75be53
Rubén López Herrera <ruben@iMac-de-Ruben.local>
2015-10-02 11:11:55 +0200

    PAS

--- xml/jefe/pas.php
+++ xml/jefe/pas.php
@@ -76,7 +76,7 @@ include("../../menu.php");
 										}
 										
 										// Administracion = 7;
-										if($pas_departamento == 'Administrativo/a' || $pas_departamento == 'Auxiliar administrativo/a') {
+										if($pas_departamento == 'Administrativo/a' || $pas_departamento == 'Auxiliar administrativo/a' || $pas_departamento == 'Titulado/a Superior') {
 											
 											$resultDepto = mysqli_query($db_con, "SELECT * FROM departamentos WHERE idea = '$pas_idea'");
 											(mysqli_num_rows($resultDepto)) ? $usuarioExiste = 1 : $usuarioExiste = 0;
